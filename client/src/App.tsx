3f047d17038f01bc3eed2e1d26c2b2724a8cbc0b
Nikita <nikita.ushackow.02@gmail.com>
2023-07-28 20:09:11 +0200

    front: working connect through GraphQL Client with Server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import Form from "./components/Form";
 import {User} from "./types/user";
 import UserItem from "./components/UserItem";
+import { gql, useQuery } from "@apollo/client";
 
 function App() {
 
@@ -9,23 +10,39 @@ function App() {
         {id: 1, email: "kenaa@example.com", username: "John Doe"},
     ]);
 
+    const GET_USERS = gql`
+        query GetUsers {
+            users {
+                id
+                email
+                username
+            }
+        }
+    `;
+
+    const { loading, error, data } = useQuery(GET_USERS);
+
     const addNewUser = (email: string, username: string) => {
         const newUser = {email, username};
+        console.log(data);
     }
 
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error : {error.message}</p>;
+
     return (
-    <div className="App">
-        <div style={{fontSize: 25}}>Hi!</div>
-        <Form
-            addNewUser={addNewUser}
-        />
-        <div>
-            <h2>List users: </h2>
-            {users?.map((user: User) =>
-                <UserItem key={user.id} user={user}/>
-            )}
+        <div className="App">
+            <div style={{fontSize: 25}}>Hi!</div>
+            <Form
+                addNewUser={addNewUser}
+            />
+            <div>
+                <h2>List users: </h2>
+                {users?.map((user: User) =>
+                    <UserItem key={user.id} user={user}/>
+                )}
+            </div>
         </div>
-    </div>
   );
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
 import App from "./App";
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
 
 import './styles/index.css'
 
+const client = new ApolloClient({
+    uri: 'http://localhost:5000/graphql',
+    cache: new InMemoryCache(),
+});
+
 const container = document.getElementById("root")!;
 const root = createRoot(container);
-root.render(<App/>);
+root.render(
+    <ApolloProvider client={client}>
+        <App/>
+    </ApolloProvider>
+);
